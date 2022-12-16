71d3905b17f848bc80f4a4ac0d79628101e87ed9
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2022-12-16 17:50:48 +0800

    api 2.2.2 7

--- src/main/java/NoMathExpectation/cs307/project2/interfaces/DBManipulation.java
+++ src/main/java/NoMathExpectation/cs307/project2/interfaces/DBManipulation.java
@@ -8,6 +8,7 @@ import org.jetbrains.annotations.Nullable;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBManipulation implements IDatabaseManipulation{
@@ -147,9 +148,78 @@ public class DBManipulation implements IDatabaseManipulation{
         return getInt(GET_SHIP_COUNT);
     }
 
+    private static final String GET_ITEM_INFO = """
+            select retcity.name        as retrival_city,
+                   delcity.name        as delivery_city,
+                   retcourier.name     as retrival_courier,
+                   delcourier.name     as delivery_courier,
+                   icity.name          as import_city,
+                   ocity.name          as export_city,
+                   i.officer           as import_officer,
+                   o.officer           as export_officer,
+                   i.tax               as import_tax,
+                   o.tax               as export_tax,
+                   it.name             as item_name,
+                   it.class            as item_class,
+                   it.price            as item_price,
+                   shipping.item_state as item_state
+            from shipping
+                     join retrieval ret on shipping.id = ret.shipping_id
+                     join delivery del on shipping.id = del.shipping_id
+                     join city delcity on delcity.id = del.city_id
+                     join city retcity on retcity.id = ret.city_id
+                     join staff retcourier on ret.courier_id = retcourier.id
+                     join staff delcourier on del.courier_id = delcourier.id
+                     join import i on shipping.import_id = i.id
+                     join export o on shipping.export_id = o.id
+                     join city icity on i.city_id = icity.id
+                     join city ocity on o.city_id = ocity.id
+                     join item it on shipping.item_id = it.id
+            where it.name = ?
+            """;
     @Override
     public ItemInfo getItemInfo(LogInfo log, String name) {
-        return null;
+        if (!checkLog(log, LogInfo.StaffType.SustcManager)) {
+            return null;
+        }
+
+        try (Connection connection = source.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(GET_ITEM_INFO)) {
+                ps.setString(1, name);
+                ResultSet rs = ps.executeQuery();
+                if (!rs.next()) {
+                    return null;
+                }
+
+                return new ItemInfo(
+                        rs.getString("item_name"),
+                        rs.getString("item_class"),
+                        rs.getDouble("item_price"),
+                        ItemState.valueOf(rs.getString("item_state")),
+                        new ItemInfo.RetrievalDeliveryInfo(
+                                rs.getString("retrival_city"),
+                                rs.getString("retrival_courier")
+                        ),
+                        new ItemInfo.RetrievalDeliveryInfo(
+                                rs.getString("delivery_city"),
+                                rs.getString("delivery_courier")
+                        ),
+                        new ItemInfo.ImportExportInfo(
+                                rs.getString("import_city"),
+                                rs.getString("import_officer"),
+                                rs.getDouble("import_tax")
+                        ),
+                        new ItemInfo.ImportExportInfo(
+                                rs.getString("export_city"),
+                                rs.getString("export_officer"),
+                                rs.getDouble("export_tax")
+                        )
+                );
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            return null;
+        }
     }
 
     @Override
